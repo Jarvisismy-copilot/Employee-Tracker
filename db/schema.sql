dd78d2d829571366ec79e4674efa214a2a5dd867
Chelsea Jarvis <chelseajarvis@MacBook-Pro-2.local>
2024-07-17 16:38:42 -0600

    added schema and seeds.sql data

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,31 @@
-﻿
+﻿-- departments table
+CREATE TABLE department (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL
+);
+
+-- roles table
+CREATE TABLE role (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL,
+    CONSTRAINT fk_department
+        FOREIGN KEY(department_id) 
+        REFERENCES department(id)
+);
+
+-- employees table
+CREATE TABLE employee (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER,
+    CONSTRAINT fk_role
+        FOREIGN KEY(role_id) 
+        REFERENCES role(id),
+    CONSTRAINT fk_manager
+        FOREIGN KEY(manager_id) 
+        REFERENCES employee(id)
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,30 @@
-﻿
+﻿-- Insert Departments
+INSERT INTO department (id, name) VALUES
+(1, 'HR'),
+(2, 'Engineering'),
+(3, 'Marketing'),
+(4, 'Operations'),
+(5, 'Customer Relations');
+
+-- Insert Roles
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Software Engineer', 70000.00, 2),
+('Marketing Director', 120000.00, 1),
+('Marketing Specialist', 60000.00, 3),
+('Sales Manager', 80000.00, 1),
+('Sales Assistant', 50000.00, 3),
+('HR Manager', 90000.00, 3),
+('Operations Manager', 120000.00, 4),
+('Customer Relations Specialist', 75000.00, 5);
+
+-- Insert Employees
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+('Jane', 'Brown', 1, NULL),
+('Benjamin', 'Crews', 1, NULL),
+('Diego', 'Garcia', 2, 1),
+('Donald', 'Groves', 3, 1),
+('Joshua', 'Cable', 1, NULL),
+('Brandon', 'Kingston', 2, 1),
+('Barbara', 'Green', 2, 1);
