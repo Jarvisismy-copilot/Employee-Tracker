8a89ddff5a9c2cf4171b8a8169c46f255bd42b6e
Chelsea Jarvis <chelseajarvis@MacBook-Pro-2.local>
2024-07-17 17:35:55 -0600

    added test features bug for index.js

--- controllers/index.js
+++ controllers/index.js
@@ -75,3 +75,15 @@ async function main() {
 }
 
 main().catch(err => console.error(err));
+
+
+
+require('dotenv').config();
+
+const client = new Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
